255f87f65f8d92681afbb2d7fee5af87a6e9252b
Andrew Van Tassel <andrew@andrewvantassel.com>
2014-05-17 16:03:34 -0600

    Moved new to index

--- index.php
+++ index.php
@@ -1,98 +1,144 @@
 
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" data-useragent="Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>GovSAFE - Survivor Assistance Form Editor</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SAFE | Survivor Assistance Form Editor</title>
 
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    
+    <meta name="description" content="Survivor Assistance Form Editor" />
+    
+    <meta name="author" content="GovSafe, lls" />
+    <meta name="copyright" content="GovSafe, llc. Copyright (c) <?=date('Y')?>" />
 
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
-	
-	<link href="jumbotron-narrow.css" rel="stylesheet">
-
-	<link href="app.css" rel="stylesheet">
-
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <link rel="stylesheet" href="css/foundation.min.css" />
+    <link rel="stylesheet" href="css/app.css" />
+    <script src="js/vendor/modernizr.js"></script>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDsbzoLjGocnaRHZF3IBMFVI-X41vPl6qM&sensor=true"></script>
   </head>
-  <body>    
-<div class="container">
-      <div class="header">
-        <ul class="nav nav-pills pull-right">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Track</a></li>
+  <body>
+    
+
+ <div class="row">
+    <div class="large-12 columns">
+ 
+      <!-- Navigation -->
+ 
+        <ul class="button-group">
+          <li><a href="#" class="button">Nav Item 1</a></li>
+          <li><a href="#" class="button">Nav Item 2</a></li>
+          <li><a href="#" class="button">Nav Item 3</a></li>
         </ul>
-        <h3 class="text-muted">Survivor Assistance Form Editor</h3>
-      </div>
+ 
+      <!-- End Navigation -->
+ 
+      <!-- Header Content -->
+ 
+        <div class="row panel">
+ 
+          <div class="large-4 columns">
+ 
+              <h4 class="hide-for-small"><img src="/images/safe-logo-simple.png" width="200"/></h4>                          
+            
+          </div>
 
-      <div class="jumbotron">
-        <h1><img src="/images/safe-logo-simple.png" width="100"/><br/>SAFE</h1>
-        <p class="lead">
-			SAFE is the Survivor Assistance Form Editor
-		</p>
-		<h2>One form to start state assistance.</h2><br/>
-		<p>
-			<a class="typeform-share btn btn-lg btn-success hide" id="start-form" href="https://avantassel.typeform.com/to/ToheBD" data-mode="1" target="_blank">Get Started</a>
-			<div id="maparea">
-				<i class='icon-spinner icon-spin icon-large'></i>
-			</div>
-		</p>
-      </div>
+          <div class="large-8 columns">
+            
+            <p>
+                SAFE is the Survivor Assistance Form Editor
+            </p>
+            <h2>One form to start state assistance.</h2>
 
-      <div class="row marketing">
-        <div class="col-lg-6">
-          <h4>Food Assistance</h4>
-          <p></p>
-
-          <h4>Material Assistance</h4>
-          <p>(Clothing, Toiletries)</p>
-
-          <h4>Housing</h4>
-          <p>(Homeowner/Renter, Damaged/Destroyed, Inaccessible?) </p>
+            <h5 class="subheader"></h5>
+            </div>
           
         </div>
-
-        <div class="col-lg-6">
-          <h4>Health/Medical</h4>
-          <p></p>
-
-          <h4>Clean-up/Re-building</h4>
-          <p></p>
-
-          <h4>Financial</h4>
-          <p></p>
-
-          <h4>Pets</h4>
-          <p>(Food, shelter, other?) </p>
+ 
+      <!-- End Header Content -->
+ 
+ 
+      <!-- Steps -->
+ 
+        <div class="row steps">
+ 
+          <div class="large-3 small-12 columns">
+            <ul class="pricing-table">
+              <li class="title step1"><span class="step">1</span> Locate</li>
+              <li class="description">Get your current location</li>
+              <li class="cta-button"><a class="button" id="locate" href="#">Locate You</a></li>
+            </ul>
+          </div>
+ 
+          <div class="large-3 small-12 columns">
+            <ul class="pricing-table">
+              <li class="title step2"><span class="step">2</span> Center</li>
+              <li class="description">Choose an assistence center</li>
+              <li class="cta-button"><a class="button" href="#">Choose One</a></li>
+            </ul>
+          </div>
+ 
+          <div class="large-3 small-12 columns">
+            <ul class="pricing-table">
+              <li class="title step3"><span class="step">3</span> Form</li>
+              <li class="description">Fill out one form</li>
+              <li class="cta-button"><a class="typeform-share button" id="start-form" href="https://avantassel.typeform.com/to/ToheBD" data-mode="1" target="_blank">Get Started</a></li>
+            </ul>
+          </div>
+ 
+          <div class="large-3 small-12 columns">
+            <ul class="pricing-table">
+              <li class="title step4"><span class="step">4</span> Status</li>
+              <li class="description">Track the status with the state</li>
+              <li class="cta-button"><a class="button" href="#">Track</a></li>
+            </ul>
+          </div>
+ 
         </div>
-        
-      </div>
-
-      <div class="footer">
-        <p>&copy; Company <?=date('Y')?></p>
-      </div>
-
-    </div> <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-  </body>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+ 
+      <!-- End Steps -->
+ 
+          <div class="row"> 
+            <div class="large-12 columns">
+              <div class="radius panel">
+                <div id="maparea">
+                  <img src="images/spinner.gif"/> Getting your Location...
+                </div>            
+            </div> 
+            </div>
+          </div>
+ 
+      <!-- Footer -->
+ 
+        <footer class="row">
+        <div class="large-12 columns"><hr />
+            <div class="row">
+ 
+              <div class="large-6 columns">
+                  <p>&copy; <?=date('Y')?></p>
+              </div>
+ 
+              <div class="large-6 columns">
+                  <ul class="inline-list right">
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                    <li><a href="#">Link 4</a></li>
+                  </ul>
+              </div>
+ 
+            </div>
+        </div>
+      </footer>
+ 
+      <!-- End Footer -->
+ 
+ 
+    </div>
+  </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="../js/foundation.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="js/jquery.geolocation.js"></script>
     <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}id=id+'_';if(!gi.call(d,id)){qs=ce.call(d,'link');qs.rel='stylesheet';qs.id=id;qs.href=b+'share-button.css';s=gt.call(d,'head')[0];s.appendChild(qs,s)}})()</script>
@@ -100,24 +146,38 @@
 
     $(document).ready(function () {
  
-    $.geolocation(function (lat, lng) {
-        var myLatlng = new google.maps.LatLng(lat, lng);
-        var mapOptions = {
-            center: new google.maps.LatLng(lat, lng),
-            zoom: 13
-        };
-        var map = new google.maps.Map(document.getElementById("maparea"), mapOptions);
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            title: "Your Location"
-        });
-        $href = $('#start-form').attr('href');
-        $('#start-form').attr('href',$href+'?ll='+lat+','+lng);
-        $('#start-form').removeClass('hide');
+    locate();
+
+    $('#locate').on('click',function(){
+      locate();
     });
+
+    function locate(){
+      $.geolocation(function (lat, lng) {
+          var myLatlng = new google.maps.LatLng(lat, lng);
+          var mapOptions = {
+              center: new google.maps.LatLng(lat, lng),
+              zoom: 13
+          };
+          var map = new google.maps.Map(document.getElementById("maparea"), mapOptions);
+          var marker = new google.maps.Marker({
+              position: myLatlng,
+              map: map,
+              title: "Your Location"
+          });
+          $href = $('#start-form').attr('href');
+          $('#start-form').attr('href',$href+'?ll='+lat+','+lng);
+          $('#start-form').removeClass('hide');
+
+          $('.step1 span').addClass('done');
+      });
+    }
 });
 
+    </script>
+    
+    <script>
+      $(document).foundation();      
     </script>
   </body>
 </html>
