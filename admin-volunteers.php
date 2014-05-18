fe71eb1b5b72441c860f363f369a5358424a01d1
Andrew Van Tassel <andrew@andrewvantassel.com>
2014-05-18 00:50:22 -0600

    Added admin reload page every 500ms

--- admin-volunteers.php
+++ admin-volunteers.php
@@ -140,7 +140,6 @@ if(!empty($api_response))
     </div>
   </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="../js/foundation.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>   
+    <script src="../js/foundation.min.js"></script>    
   </body>
 </html>
