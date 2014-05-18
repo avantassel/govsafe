4cff191b82b01542b17f29c3a08e3d4f55b1ecc5
Andrew Van Tassel <andrew@andrewvantassel.com>
2014-05-18 11:34:36 -0600

    Testing out since but typeform not workin

--- getsurvivors.php
+++ getsurvivors.php
@@ -1,6 +1,8 @@
 <?php
 
 $api_url = 'https://api.typeform.com/v0/form/ToheBD?key=06e283cf9a2f96c5674821ef2335742b8f4a362b&completed=true';
+if(!empty($_GET['since']))
+	$api_url.='&since='.$_GET['since'];
 $api_response=@file_get_contents($api_url);
 
 header("Content-type: application/json");
